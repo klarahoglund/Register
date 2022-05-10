3d91514dfa66558d056fc89af3072349a07f7e3e
Klara Höglund <klarahoglund@hotmail.com>
2022-05-10 16:48:20 +0200

    Add project files.

---
+++ Register/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+List<worker> register = new List<worker>();
+
+
+Console.WriteLine("Välkommen till Registret");
+
+
+Console.WriteLine("Vill du lägga till en anställd, tryck a.Vill du skriva ut, tryck s");
+string valet = Console.ReadLine();
+
+if (valet== "a")
+{
+    addWorker();
+   
+    
+}
+else if (valet== "")
+{
+    skrivUt();
+}
+
+void skrivUt()
+{
+    foreach (var worker in register)
+    {
+        Console.WriteLine(worker);  
+    }
+}
+
+void addWorker()
+{
+    Console.WriteLine("Lön: ");
+
+    string hold = Console.ReadLine();
+
+    int salary = Convert.ToInt32(hold);
+    Console.WriteLine("Namn: ");
+
+    string name = Console.ReadLine();
+
+    if (name.Length > 0)
+    {
+        worker worker = new(name, salary);
+        register.Add(worker);
---
+++ Register/worker.cs
@@ -0,0 +1,12 @@
+﻿public class worker
+{
+    public worker(string name, int salary)
+    {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public string name { get; set; } 
+    public int salary { get; set; }
+
+}
